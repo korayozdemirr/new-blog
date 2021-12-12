555ccbf08ce586fd1d96e33ad3d1688fe9cbb0fe
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2021-12-12 19:35:37 +0300

    video-2

---
+++ pages/blog.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function blog() {
+  return <div className="site-container">merhaba</div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            Ben Koray Özdemir. Kırklareli'de yaşayan Dijital Ürün Tasarımcısıyım
+          </h1>
+          <p>
+            Tasarım araçları, front-end teknolojileri gibi konularla yakından
+            ilgileniyorum
+          </p>
+          <p>
+            Tasarım araçları, front-end teknolojileri gibi konularla yakından
+            ilgileniyorum
+          </p>
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto mt-4">
+        <img className="w-full" src="/profil.jpg" alt="profil" />
+      </div>
+    </>
+  )
 }
 
 export default HomePage
